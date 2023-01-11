5df64f09e1f454ad6e96b34affbb343939a65be5
upend1310 <shawkumarupendra@outlook.com>
2023-01-11 18:34:42 -0500

    Added tuples.py

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# Tuples are similar to lists but immutable
+coordinates = (4, 5)
+print(coordinates[0])
+# coordinates[1] = 10
+# we can crate a list of tuples
+new_coordinates = [(4, 5), (6, 7)]
+print(new_coordinates)
+
+
