5f83687f52f3796358c7b4ddd9b986f44c190816
upend1310 <shawkumarupendra@outlook.com>
2023-01-11 20:40:35 -0500

    updated

---
+++ modulesAndPip.py
@@ -0,0 +1,7 @@
+import usefulTools
+
+print(usefulTools.roll_dice(10))
+
+# We can use pip to install external modules
+# there are some build in modules which are present in Lib
+# for 3rd party modules use pip and then can be seen under site-packages
