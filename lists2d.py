bf2587e18c1607bf7c10a9167c24bea16f19a338
upend1310 <shawkumarupendra@outlook.com>
2023-01-11 20:09:34 -0500

    updated

---
+++ lists2d.py
@@ -0,0 +1,27 @@
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+print(number_grid[0][0])
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
