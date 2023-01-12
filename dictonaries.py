b788d1b854e745d1c49141bc93c7c7b2e5c5c629
upend1310 <shawkumarupendra@outlook.com>
2023-01-11 19:16:00 -0500

    Added calculator.py

---
+++ dictonaries.py
@@ -0,0 +1,34 @@
+# used to create a key value pair in python
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+monthConversionsWithNumber = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+}
+print(monthConversions["Nov"])
+print(monthConversions.get("Dec"))
+print(monthConversions.get("Luv"))
+print(monthConversions.get("Luv", "Not a valid key!"))
